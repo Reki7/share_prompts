36e0e3d59d37cdfcd543ebc6262bc87d957fd815
svs <promissum@gmail.com>
2023-05-24 15:23:22 +0300

    Connect DB: signIn() & User.model

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from 'next-auth/providers';
 import { connectToDB } from '@utils/database';
+import User from '@models/user';
 // const NextAuth = require("next-auth");
 
 const handler = NextAuth({
@@ -15,8 +16,16 @@ const handler = NextAuth({
     try {
       await connectToDB();
       // check if a user already exists
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
 
       // if not, create a new user
+      await User.create({
+        email: profile.email,
+        username: profile.username.replace(' ', '').toLowerCase(),
+        image: profile.picture,
+      })
       return true;
     } catch (e) {
       console.log(e);
