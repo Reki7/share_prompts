9e81161ce43ac14ea8c9273e52d80a3b96e02007
svs <promissum@gmail.com>
2023-05-29 22:14:03 +0300

    Nexauth provider.callbacks fix

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -11,32 +11,34 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     })
   ],
-  async session({session}) {
-    const sessionUser = await User.findOne({
-      email: session.user.email,
-    });
-    session.user.id = sessionUser._id.toSigned();
-    return session;
-  },
-  async signIn({profile}) {
-    try {
-      await connectToDB();
-      // check if a user already exists
-      const userExists = await User.findOne({
-        email: profile.email,
+  callbacks: {
+    async session({session}) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
       });
+      session.user.id = sessionUser._id.toSigned();
+      return session;
+    },
+    async signIn({profile}) {
+      try {
+        await connectToDB();
+        // check if a user already exists
+        const userExists = await User.findOne({
+          email: profile.email,
+        });
 
-      // if not, create a new user
-      await User.create({
-        email: profile.email,
-        username: profile.username.replace(' ', '').toLowerCase(),
-        image: profile.picture,
-      })
-      return true;
-    } catch (e) {
-      console.log(e);
-      return false;
-    }
+        // if not, create a new user
+        await User.create({
+          email: profile.email,
+          username: profile.username.replace(' ', '').toLowerCase(),
+          image: profile.picture,
+        })
+        return true;
+      } catch (e) {
+        console.log(e);
+        return false;
+      }
+    },
   },
 });
 
